403cd0286c3103f6e67ce2d65f89f69f2e874f96
Ananta Asim Joy <anantajoy007@gmail.com>
2020-09-12 00:19:05 +0600

    c++ by cherno

---
+++ C++ Programming/the_cherno/Basics/header.h
@@ -0,0 +1,4 @@
+int main(){
+    std::cout << "Hello World" << std::endl;
+}
+// you can create your own header file
---
+++ C++ Programming/the_cherno/Variables/variable.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    // integer type
+    int age = 21;
+    std::cout<<age<<std::endl;
+    // float/double 
+    float weight = 65.5;
+    std::cout << weight << std::endl;
+    // character
+    char letter = 'A';
+    std::cout << letter << std::endl;
+    // ascii
+    // int letters = '65';
+    // std::cout << letter << std::endl;
+    // boolean
+    bool is_true = true;
+    std::cout << is_true << std::endl;
+    // size of a data types
+    std::cout << sizeof(bool) << std::endl;
+    return 0;
+}
