92e94cb39da39ff9200d91072378171718a6f25d
Ananta Asim Joy <anantajoy007@gmail.com>
2020-09-15 23:00:56 +0600

    pi_million.txt added

---
+++ C++ Programming/the_cherno/Conditional/if_else.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    bool is_rain = true;
+    if (is_rain==true)
+    {
+        cout << "Take a RainCoat";
+    }
+    else
+    {
+        cout << "Wear a t-shirt";
+    }
+    
+    
+    return 0;
+}
